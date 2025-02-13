9b821c88a14afeec74e1d3c26a5c2a6bb64838d9
mgumbar <mgumbar@users.noreply.github.com>
2025-02-13 07:25:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 console.log("Scheduler démarré:");
 
 // Planifier l'exécution toutes les 30 minutes
-cron.schedule('*/1 * * * *', () => {
+cron.schedule('*/60 * * * *', () => {
     console.log('Exécution du script UpdateStrongBuyFavoriteService.js');
     exec('node UpdateStrongBuyFavoriteService.js', (error, stdout, stderr) => {
         if (error) {
