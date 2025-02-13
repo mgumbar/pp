e204bf6854cd31421ff1f692b70c21725dcfdc57
mgumbar <mgumbar@users.noreply.github.com>
2025-02-13 13:08:48 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: true }); // Mode headless
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com', { waitUntil: 'networkidle2' });
+    
+    // Prendre la capture d'écran
+    await page.screenshot({ path: 'google_screenshot.png' });
+    
+    console.log('Capture d’écran enregistrée sous google_screenshot.png');
+    
+    await browser.close();
+})();
