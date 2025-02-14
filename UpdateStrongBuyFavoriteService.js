744055aa9cfa2f874bcfa926c9569e6b5ca8cb40
mgumbar <mgumbar@users.noreply.github.com>
2025-02-14 21:39:27 +0100

    Update UpdateStrongBuyFavoriteService.js

--- UpdateStrongBuyFavoriteService.js
+++ UpdateStrongBuyFavoriteService.js
@@ -14,9 +14,17 @@ async function run() {
     const collectionIndices = db.collection('indices');
     console.log('Connected to MongoDB');
     const browser = await puppeteer.launch({ 
-    executablePath: '/usr/bin/chromium-browser', 
-    headless: true, 
-    args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+        executablePath: '/usr/bin/chromium-browser',
+    headless: true,
+            args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-crash-reporter',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-software-rasterizer',
+      '--disable-extensions'
+    ]
 });
     const page = await browser.newPage();
 
