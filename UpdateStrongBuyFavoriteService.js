d8c84e228b4dcfd6ee1d9b3c404d9d553b93e18f
mgumbar <mgumbar@users.noreply.github.com>
2025-02-14 21:43:17 +0100

    Update UpdateStrongBuyFavoriteService.js

--- UpdateStrongBuyFavoriteService.js
+++ UpdateStrongBuyFavoriteService.js
@@ -5,6 +5,30 @@ const uri = process.env.MONGO_URI;
 const codes = process.env.CODES.split(',');
 console.log(codes);
 
+(async () => {
+    const browser = await puppeteer.launch({ 
+        executablePath: '/usr/bin/chromium-browser',
+    headless: true,
+            args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-crash-reporter',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-software-rasterizer',
+      '--disable-extensions'
+    ]
+});
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com', { waitUntil: 'networkidle2' });
+    
+    // Prendre la capture d'écran
+    await page.screenshot({ path: 'google_screenshot.png' });
+    
+    console.log('Capture d’écran enregistrée sous google_screenshot.png');
+    
+    await browser.close();
+})();
 
 async function run() {
     const client = new MongoClient(uri);
