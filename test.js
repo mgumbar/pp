fdabe07204c381403223b9046c868cfd9cc5e306
mgumbar <mgumbar@users.noreply.github.com>
2025-02-14 21:35:02 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({ 
+        executablePath: '/usr/bin/chromium-browser',
     headless: true,
             args: [
       '--no-sandbox',
