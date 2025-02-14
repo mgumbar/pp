6ab2046c3d9c259b80a0ab33373a3cae2f5c0922
mgumbar <mgumbar@users.noreply.github.com>
2025-02-14 21:49:27 +0100

    Update UpdateStrongBuyFavoriteService.js

--- UpdateStrongBuyFavoriteService.js
+++ UpdateStrongBuyFavoriteService.js
@@ -5,33 +5,9 @@ const uri = process.env.MONGO_URI;
 const codes = process.env.CODES.split(',');
 console.log(codes);
 
+
+//async function run() {
 (async () => {
-    const browser = await puppeteer.launch({ 
-        executablePath: '/usr/bin/chromium-browser',
-    headless: true,
-            args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-crash-reporter',
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--disable-software-rasterizer',
-      '--disable-extensions'
-    ]
-});
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com', { waitUntil: 'networkidle2' });
-    
-    // Prendre la capture d'écran
-    await page.screenshot({ path: 'google_screenshot.png' });
-    
-    console.log('Capture d’écran enregistrée sous google_screenshot.png');
-    
-    await browser.close();
-})();
-
-
-async function run() {
     const client = new MongoClient(uri);
     await client.connect();
     const db = client.db('marketData');
@@ -168,9 +144,10 @@ async function run() {
     } finally {
         await browser.close();
     }
-}
+})();
+    //}
 
-run();
+//run();
 
 
 function convertToDecimal(inputString) {
