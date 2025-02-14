f111053cac0f3c7e1c48399bf8459d725acf8fdd
mgumbar <mgumbar@users.noreply.github.com>
2025-02-14 21:16:50 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,15 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({ 
-    headless: true
+    headless: true,
+            args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-software-rasterizer',
+      '--disable-extensions'
+    ]
 });
     const page = await browser.newPage();
     await page.goto('https://www.google.com', { waitUntil: 'networkidle2' });
