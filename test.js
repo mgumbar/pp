894a9970d0b4f2e84bc678c8d4de6be6f4fb7608
mgumbar <mgumbar@users.noreply.github.com>
2025-02-15 08:52:34 +0100

    Update test.js

--- test.js
+++ test.js
@@ -16,7 +16,7 @@ const puppeteer = require('puppeteer-core');
     ]
 });
 */
-      const browserURL = 'http://172.17.0.4:3000';
+      const browserURL = 'https://46.101.203.58:3000';
   
   // Se connecter au navigateur externe
   const browser = await puppeteer.connect({ browserURL });
