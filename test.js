42a7277d73a69d51f2ba951906e72ebe92b13f97
mgumbar <mgumbar@users.noreply.github.com>
2025-02-15 08:55:26 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,25 +2,29 @@ const puppeteer = require('puppeteer-core');
 
 (async () => {
     /*
+    // Exemple de lancement d'une nouvelle instance de navigateur avec puppeteer.launch()
     const browser = await puppeteer.launch({ 
         executablePath: '/usr/bin/chromium-browser',
-    headless: true,
-            args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-crash-reporter',
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--disable-software-rasterizer',
-      '--disable-extensions'
-    ]
-});
-*/
-      const browserURL = 'http://46.101.203.58:3000';
-  
-  // Se connecter au navigateur externe
-  const browser = await puppeteer.connect({ browserURL });
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-crash-reporter',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-software-rasterizer',
+            '--disable-extensions'
+        ]
+    });
+    */
+
+    // Adresse de l'instance du navigateur externe lancé avec --remote-debugging-port
+    const browserURL = 'http://46.101.203.58:3000';
     
+    // Se connecter au navigateur externe
+    const browser = await puppeteer.connect({ browserURL });
+    
+    // Ouvrir une nouvelle page et naviguer
     const page = await browser.newPage();
     await page.goto('https://www.google.com', { waitUntil: 'networkidle2' });
     
@@ -29,5 +33,6 @@ const puppeteer = require('puppeteer-core');
     
     console.log('Capture d’écran enregistrée sous google_screenshot.png');
     
+    // Fermer la connexion (ne ferme pas nécessairement le navigateur externe)
     await browser.close();
 })();
